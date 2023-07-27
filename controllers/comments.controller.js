25498608175f14235a6bac18b0b04928173b5768
Khamz <khamzzaty@mail.ru>
2023-07-27 16:03:53 +0300

    Comments 2

---
+++ controllers/comments.controller.js
@@ -0,0 +1,40 @@
+const Comments = require('../models/Comments.model');
+// Sozdanie kontrollera commentov
+module.exports.commentsController={
+    getComments: async(req, res)=>{
+        try {
+            const comments = await Comments.find({})
+            res.json(comments)
+        } catch (error) {
+            res.json(error)
+        }
+    },
+    postComments: async(req, res)=>{
+        const {text,user} = req.body
+        // const {user}=req.user.id
+        try {
+            
+            const comments = await Comments.create({
+                text,
+                user
+            })
+            res.json(comments)
+        } catch (error) {
+            res.json(error)
+        }
+    },
+    deleteComments: async (req, res)=>{
+        const {id}=req.params
+        try {
+            const comments = await Comments.findById({id});
+            if(comments.user.toString()===req.user.id){
+                Comments.findByIdAndDelete(id)
+              return  res.json('Комментарий удалён')
+            } 
+            return res.status(401).json('Вы не можете удалить чужой комментарий')
+           
+        } catch (error) {
+            res.json(error)
+        }
+    }
+} 
